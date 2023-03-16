e4665959677479b8d4502960f22e257583bf4364
BambicoIsaac <67004532+BambicoIsaac@users.noreply.github.com>
2023-03-16 17:08:32 +0800

    Add files via upload

---
+++ AssessmentTask/clockLesserAngle/clockLesserAngle.cs
@@ -0,0 +1,71 @@
+﻿//A C# Console App wherein the user
+//inputs the hours and minutes of the analogue
+//clock.
+
+//The program calculates the lesser angle in degrees
+//between the hour hand and the minute hand and
+//outputs the answer in the console window.
+
+using System;
+
+class clockAngle
+{
+
+    //Function to calculate the angle
+    static float calcAngle(double h, double m)
+    {
+        //Input Validation
+        if (h < 0 || m < 0 ||
+            h > 12 || m > 60)
+            Console.Write("Invalid input.");
+
+        //Set 12:00 as origin
+        if (h == 12)
+            h = 0;
+
+        //If user inputs minutes as 60, automatically calculate
+        //it as the next hour and set minute at 0 instead.
+        if (m == 60)
+        {
+            m = 0;
+            h += 1;
+
+            //If this calculation exceeds 12 (12:60),
+            //Convert hour to 1
+            if (h > 12)
+                h = h - 12;
+        }
+
+        //The hour hand moves 30 degrees every hour 
+        //and 0.5 degrees every minute.
+        float hour_angle = (float)((30 * h) + (0.5 * m));
+
+        //Minute hand moves 6 degrees every minute.
+        float minute_angle = (float)(6 * m);
+
+        //Find the difference between two angles
+        //in absolute value.
+        float angle = Math.Abs(hour_angle - minute_angle);
+
+        //We are looking for the lesser angle so we
+        //take the smaller value between two possible angles
+        //(Hour Hand to Minute Hand) vs (Minute Hand to Hour Hand)
+        angle = Math.Min(360 - angle, angle);
+
+        return angle;
+    }
+
+    //Driver Code
+    public static void Main()
+    {
+        Console.Write("Input Hour: ");
+        string stringHour = Console.ReadLine();
+        int inputHour = Convert.ToInt32(stringHour);
+        Console.Write("Input Minutes: ");
+        string stringMins = Console.ReadLine();
+        int inputMins = Convert.ToInt32(stringMins);
+        Console.WriteLine("The lesser angle for the time " + stringHour
+            + ":" + stringMins + " is " + calcAngle(inputHour, inputMins)
+            + " degrees.");
+    }
+}
---
+++ AssessmentTask/structureDepth/structureDepth.cs
@@ -0,0 +1,114 @@
+﻿//A C# program that allows the user to create
+//the structure using the driver code
+//to which the program then computes
+//for the depth of the structure.
+
+class BranchStructure
+{
+    public class Branch
+    {
+        public string value;
+        public List<Branch> branches;
+    };
+
+    //Function to add a new branch
+    static Branch newBranch(string value)
+    {
+        Branch temp = new Branch();
+        temp.value = value;
+        temp.branches = new List<Branch>();
+        return temp;
+    }
+
+    //Function to find the depth of the structure
+    public static int depthOfStructure(Branch tree)
+    {
+        int depth = 0;
+        if (tree == null)
+            return depth;
+
+        // Create a queue to traverse the structure
+        Queue<Branch> q = new Queue<Branch>();
+
+        // Enqueue whole structure to the queue
+        q.Enqueue(tree);
+
+        while (q.Count != 0)
+        {
+            int n = q.Count;
+
+            //Check to see if the current branch still
+            //has branches
+            while (n > 0)
+            {
+                // Dequeue an item from queue
+                Branch p = q.Peek();
+                q.Dequeue();
+
+                // Enqueue all branch of
+                // the dequeued branch
+                for (int i = 0; i < p.branches.Count; i++)
+                    q.Enqueue(p.branches[i]);
+                n--;
+            }
+
+            //Upon completing a structure level, increase
+            //depth counter
+            depth++;
+        }
+        return depth;
+    }
+
+    //Driver Code
+    public static void Main(String[] args)
+    {
+        /* Given Structure:
+                      A
+                    /   \
+                   B     C
+                  /    / | \
+                 D    E  F  G 
+                     /  / \
+                    H  I   J  
+                       |
+                       K
+        */
+
+        //Initialized the object as null
+        //for error handling when user
+        //decides to not create a structure
+        Branch tree = null;
+
+
+        //Building the structure using our
+        //newBranch function
+        tree = newBranch("A");
+        (tree.branches).Add(newBranch("B"));
+        (tree.branches).Add(newBranch("C"));
+        (tree.branches[0].branches).Add(newBranch("D"));
+        (tree.branches[1].branches).Add(newBranch("E"));
+        (tree.branches[1].branches).Add(newBranch("F"));
+        (tree.branches[1].branches).Add(newBranch("G"));
+        (tree.branches[1].branches[0].branches)
+            .Add(newBranch("H"));
+        (tree.branches[1].branches[1].branches)
+            .Add(newBranch("I"));
+        (tree.branches[1].branches[1].branches)
+                  .Add(newBranch("J"));
+        (tree.branches[1].branches[1].branches[0].branches)
+            .Add(newBranch("K"));
+
+        //Structure exists
+        if (tree != null)
+        {
+            Console.WriteLine("The depth of this structure"
+                + " is: " + depthOfStructure(tree));
+        }
+
+        //Structure does not exist
+        else
+        {
+            Console.WriteLine("No structure generated.");
+        }
+    }
+}
